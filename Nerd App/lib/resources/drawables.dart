b5826fe4888c057c00706489787eb73ff29b6fd8
AthiraGreejith <athirasri1720@gmail.com>
2022-02-03 14:49:50 +0530

    changed room  cards

--- Nerd App/lib/resources/drawables.dart
+++ Nerd App/lib/resources/drawables.dart
@@ -4,4 +4,7 @@ class Drawables {
   static const String logo_small = "assets/logo_small.png";
   static const String avatarSitting = "assets/avatar_sitting.png";
   static const String empty_chair = "assets/empty_chair.png";
+  static const String room100 = "assets/room100.jpg";
+  static const String room50 = "assets/room50.png";
+  static const String room15 = "assets/room15.jpg";
 }
--- Nerd App/lib/room_list.dart
+++ Nerd App/lib/room_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:nerd/resources/app_colors.dart';
 import 'package:nerd/resources/drawables.dart';
+import 'package:nerd/utils/app_text.dart';
 import 'package:provider/provider.dart';
 
 import 'controller/game_controller.dart';
@@ -17,31 +19,49 @@ class _RoomListState extends State<RoomList> {
   Widget build(BuildContext context) {
     return ListView(
       children: [
-        _offerProductsBannerView(),
+        gameRoomsBannerView(100,"Game Room 100 players",Drawables.room100),
+        gameRoomsBannerView(50,"Game Room 50 players",Drawables.room50),
+        gameRoomsBannerView(15,"Game Room 15 players",Drawables.room15),
       ],
     );
   }
 
-  Widget _offerProductsBannerView() {
+  Widget gameRoomsBannerView(int personCount,String title,String image) {
     return GestureDetector(
       onTap: () {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => GameRooms(personCount: 100),
+            builder: (context) => GameRooms(personCount: personCount),
           ),
         );
       },
-      child: Container(
+      child: Container(padding: EdgeInsets.all(16),
         width: double.infinity,
         clipBehavior: Clip.antiAlias,
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8), color: Colors.red),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 5,
+                blurRadius: 7,
+                offset: Offset(0, 3), // changes position of shadow
+              ),
+            ],
+
+            borderRadius: BorderRadius.circular(8), color:Colors.white),
         margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
         height: 140,
-        child: Image.asset(
-          Drawables.logo,
-          fit: BoxFit.cover,
+        child: Column(
+          children: [
+            Image.asset(
+              image,
+              height: 90,width: 250,
+              // color: Colors.red,
+              fit: BoxFit.contain,
+            ),
+            AppText.normal_14(title)
+          ],
         ),
       ),
     );
